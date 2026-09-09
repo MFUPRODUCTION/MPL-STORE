7076f97e975b42f69016de7fb868bba8e6b1dfba
MFUPRODUCTION <muhammadfaruqu22@gmail.com>
2026-09-09 21:29:13 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-// Publik di browser. Hanya anon/publishable key, JANGAN secret/service_role.
-globalThis.SantriPulangConfig = Object.freeze({
-  useSupabase: false,
-  supabase: {
-    url: 'https://YOUR_PROJECT.supabase.co',
-    anonKey: 'YOUR_ANON_OR_PUBLISHABLE_KEY'
-  }
-});
